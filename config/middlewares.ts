eaf63c95bf2f42b90481a738b0655036fef4f1fe
riskamufika-dot <riskamufika@gmail.com>
2026-08-20 14:42:54 +0700

    perbaikan serve

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,18 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://smart-canteen-frontend-nine.vercel.app',
+        'http://localhost:3000',
+      ],
+      headers: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
